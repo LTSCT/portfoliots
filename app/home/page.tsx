87155e3d2f8e9d9702868db4a26e29d7e579fc08
LTSCT <leytscls@gmail.com>
2025-09-12 10:48:13 +0700

    changes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,14 +13,14 @@ const phetsarathFont = Phetsarath({
 
 export default function HomePage() {
   return (
-    <div className={`${phetsarathFont.variable}`}>
-      <div className="bg-muted flex miSn-h-svh flex-col items-center justify-center p-6 md:p-10 font-main">
+    
+      <div className="bg-muted flex miSn-h-svh flex-col items-center justify-center p-6 md:p-10">
         <div className="w-full max-w-sm md:max-w-3xl">
-            <div className="container mx-auto p-8 pt-26">
-              <h1 className="mb-4 text-center text-3xl">
+            <div className={`container mx-auto p-8 pt-26${phetsarathFont.variable}`}>
+              <h1 className="mb-4 text-center text-3xl font-main">
                 ຍິນດີຕອນຮັບ, ຄົນຫລໍ່ແລະນ່າຮັກ ເຂົ້າສູ່ເຂົາວົງກົດປ່າທຶບໜາ!!
               </h1>
-              <p className="mb-10 text-center text-sm text-zinc-500">
+              <p className="mb-10 text-center text-sm text-zinc-500 font-main">
                 ເຮົາມີ ຊື່ວ່າ ເລ້ ມັກຮັກໃນການອອກແບບ, ກົດໝາຍ ແລະ ບັນດາເທັກໂນໂລຊີ{" "}
                 <span>ສຶກສາ</span>. ຮຽນຮຼ້ສິ່ງໃໝ່ຽທາງດ້ານ ວິທະຍາສາດການສຶກສາ
                 ສິ່ງທີ່ທັນສະໃໝ່<span>ຍຸກແຫຼ່ງ</span> Ai.
@@ -31,6 +31,6 @@ export default function HomePage() {
           <Centerhome />
           <Cardworkhome />
       </div>
-    </div>
+    
   )
 }
