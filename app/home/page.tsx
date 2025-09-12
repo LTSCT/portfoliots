63424114668bcadc1495407a088ae5949406039b
LTSCT <leytscls@gmail.com>
2025-09-12 10:40:21 +0700

    changes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -14,23 +14,23 @@ const phetsarathFont = Phetsarath({
 export default function HomePage() {
   return (
     <div className={`${phetsarathFont.variable}`}>
-    <div className="bg-muted flex miSn-h-svh flex-col items-center justify-center p-6 md:p-10 font-main">
-      <div className="w-full max-w-sm md:max-w-3xl">
-          <div className="container mx-auto p-8 pt-26 font-main">
-            <h1 className="mb-4 text-center text-3xl">
-              ຍິນດີຕອນຮັບ, ຄົນຫລໍ່ແລະນ່າຮັກ ເຂົ້າສູ່ເຂົາວົງກົດປ່າທຶບໜາ!!
-            </h1>
-            <p className="mb-10 text-center text-sm text-zinc-500">
-              ເຮົາມີ ຊື່ວ່າ ເລ້ ມັກຮັກໃນການອອກແບບ, ກົດໝາຍ ແລະ ບັນດາເທັກໂນໂລຊີ{" "}
-              <span className="font-medium">ສຶກສາ</span>. ຮຽນຮຼ້ສິ່ງໃໝ່ຽທາງດ້ານ ວິທະຍາສາດການສຶກສາ
-              ສິ່ງທີ່ທັນສະໃໝ່<span className="font-medium">ຍຸກແຫຼ່ງ</span> Ai.
-            </p>
-          </div>
+      <div className="bg-muted flex miSn-h-svh flex-col items-center justify-center p-6 md:p-10 font-main">
+        <div className="w-full max-w-sm md:max-w-3xl">
+            <div className="container mx-auto p-8 pt-26">
+              <h1 className="mb-4 text-center text-3xl">
+                ຍິນດີຕອນຮັບ, ຄົນຫລໍ່ແລະນ່າຮັກ ເຂົ້າສູ່ເຂົາວົງກົດປ່າທຶບໜາ!!
+              </h1>
+              <p className="mb-10 text-center text-sm text-zinc-500">
+                ເຮົາມີ ຊື່ວ່າ ເລ້ ມັກຮັກໃນການອອກແບບ, ກົດໝາຍ ແລະ ບັນດາເທັກໂນໂລຊີ{" "}
+                <span>ສຶກສາ</span>. ຮຽນຮຼ້ສິ່ງໃໝ່ຽທາງດ້ານ ວິທະຍາສາດການສຶກສາ
+                ສິ່ງທີ່ທັນສະໃໝ່<span>ຍຸກແຫຼ່ງ</span> Ai.
+              </p>
+            </div>
+        </div>
+          <Cardhome />
+          <Centerhome />
+          <Cardworkhome />
       </div>
-        <Cardhome />
-        <Centerhome />
-        <Cardworkhome />
-    </div>
     </div>
   )
 }
